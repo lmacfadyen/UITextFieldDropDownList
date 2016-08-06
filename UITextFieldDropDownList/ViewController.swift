a81d883be492fb8afc7bee28a99f0705243f7424
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-08-06 08:56:18 -0400

    Initial Commit

---
+++ UITextFieldDropDownList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITextFieldDropDownList
+//
+//  Created by Lawrence F MacFadyen on 2016-08-06.
+//  Copyright © 2016 LawrenceM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
